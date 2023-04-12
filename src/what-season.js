16e3551d28c7369ba7bd49ae2a72ef0d4773368c
gordinski <e.gordinski@gmail.com>
2023-04-12 20:14:24 +0300

    feat: solve what-season task

--- src/what-season.js
+++ src/what-season.js
@@ -13,23 +13,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 function getSeason(date) {
   if (!date) {
-    return 'Unable to determine the time of year!'
-  };
-
-  if (!(date instanceof Date) || isNaN(date.getTime())) {
-    throw new Error('Invalid date!');
+    return 'Unable to determine the time of year!';
   }
 
-  const month = date.getMonth() + 1;
+  try {
+    date.getTime();
+  } catch (error) {
+    throw new Error("Invalid date!");
+  }
 
-  if (month === 12 || month <= 2) {
-    return 'winter';
-  } else if (month >= 3 && month <= 5) {
-    return 'spring';
-  } else if (month >= 6 && month <= 8) {
-    return 'summer';
+  const month = date.getMonth();
+
+  if (month === 11 || month <= 1) {
+    return 'winter'
+  } else if (month <= 4) {
+    return 'spring'
+  } else if (month <= 7) {
+    return 'summer'
   } else {
-    return 'autumn';
+    return 'autumn'
   }
 }
 
